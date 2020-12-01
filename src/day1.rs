697c2b9d35d7abf034f6d02984274c40bb5692f1
Owen Nelson <onelson@gmail.com>
2020-11-30 21:29:22 -0800

    Pass day 1 part 2.

--- src/day1.rs
+++ src/day1.rs
@@ -18,13 +18,36 @@ pub fn solve_part1(input: &[i32]) -> i32 {
     unreachable!()
 }
 
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &[i32]) -> i32 {
+    for x in input {
+        for y in input {
+            for z in input {
+                if x == y || x == z || y == z {
+                    continue;
+                }
+                if x + y + z == 2020 {
+                    return x * y * z;
+                }
+            }
+        }
+    }
+    unreachable!()
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::day1::solve_part1;
+    use crate::day1::{solve_part1, solve_part2};
 
     #[test]
     fn test_part1_case1() {
         let input = vec![1721, 979, 366, 299, 675, 1456];
         assert_eq!(514579, solve_part1(&input));
+    }
+
+    #[test]
+    fn test_part1_case2() {
+        let input = vec![1721, 979, 366, 299, 675, 1456];
+        assert_eq!(241861950, solve_part2(&input));
     }
 }
