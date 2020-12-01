d14f8030d00d6024c9dae38a47a88c2ab6aa08ab
Owen Nelson <onelson@gmail.com>
2020-11-30 21:23:58 -0800

    Pass for day1 part1.

--- src/day1.rs
+++ src/day1.rs
@@ -1,1 +1,30 @@
-//!
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<i32> {
+    input.lines().map(|s| s.parse().unwrap()).collect()
+}
+
+#[aoc(day1, part1)]
+pub fn solve_part1(input: &[i32]) -> i32 {
+    for x in input {
+        for y in input {
+            if x == y {
+                continue;
+            }
+            if x + y == 2020 {
+                return x * y;
+            }
+        }
+    }
+    unreachable!()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day1::solve_part1;
+
+    #[test]
+    fn test_part1_case1() {
+        let input = vec![1721, 979, 366, 299, 675, 1456];
+        assert_eq!(514579, solve_part1(&input));
+    }
+}
