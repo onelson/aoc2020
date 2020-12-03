97daf9f8805c325a37527702ad72a43446ab885c
Owen Nelson <onelson@gmail.com>
2020-12-02 20:47:28 -0800

    prep for day3

---
+++ src/day3.rs
@@ -0,0 +1,25 @@
+type Item = ();
+
+#[aoc_generator(day3)]
+pub fn input_generator(input: &str) -> Vec<Item> {
+    todo!()
+}
+
+#[aoc(day3, part1)]
+pub fn solve_part1(input: &[Item]) -> usize {
+    todo!()
+}
+
+#[aoc(day3, part2)]
+pub fn solve_part2(input: &[Item]) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{solve_part1, solve_part2, Item};
+    #[test]
+    fn test_part1_case1() {
+        todo!()
+    }
+}
