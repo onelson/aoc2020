183d3d1647aa14874bc4b86da16436096c5658a8
Owen Nelson <onelson@gmail.com>
2020-12-07 22:22:58 -0800

    prep more days

--- src/day10.rs
+++ src/day10.rs
@@ -1,16 +1,16 @@
 pub struct Item;
 
-#[aoc_generator(day9)]
+#[aoc_generator(day10)]
 pub fn input_generator(_input: &str) -> Vec<Item> {
     todo!()
 }
 
-#[aoc(day9, part1)]
+#[aoc(day10, part1)]
 pub fn solve_part1(_input: &[Item]) -> usize {
     todo!()
 }
 
-#[aoc(day9, part2)]
+#[aoc(day10, part2)]
 pub fn solve_part2(_input: &[Item]) -> usize {
     todo!()
 }
