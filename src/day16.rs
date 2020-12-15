2df289b9eab179b38e083cac4dbe9792e3c15cd0
Owen Nelson <onelson@gmail.com>
2020-12-14 22:42:56 -0800

    Prep day 16

---
+++ src/day16.rs
@@ -0,0 +1,28 @@
+pub type Item = i64;
+
+#[aoc_generator(day16)]
+pub fn input_generator(_input: &str) -> Vec<Item> {
+    todo!()
+}
+
+#[aoc(day16, part1)]
+pub fn solve_part1(_input: &[Item]) -> i64 {
+    todo!()
+}
+
+#[aoc(day16, part2)]
+pub fn solve_part2(_input: &[Item]) -> i64 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    #![allow(unused)]
+
+    use super::*;
+
+    const EXAMPLE: &str = r##""##;
+
+    #[test]
+    fn test_something() {}
+}
